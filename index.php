b0d13719b182f38cdb4f2de6f7b79b7c0679d31a
Christopher Pengalilla <christopher.pengalilla@gmail.com>
2024-06-29 23:02:50 +0700

    Fix Dark Mode Styling
    Fixing button styling and coloring in index.php page and pass `DarkMode == enabled` status to bandingkan.php. So when user enabling Dark Mode in homepage, it will affect the Compare page too.

--- index.php
+++ index.php
@@ -1,30 +1,31 @@
-<?php 
-    $arrHape = array (
-        array("SKU"=>"AS001", "Merk"=>"Samsung", "Model"=>"Galaxy S22", "Harga" => 18000000, "url_gambar"=>"https://static.retailworldvn.com/Products/Images/12220/301427/smartphone-samsung-galaxy-s22-5g-8gb-128gb-black-1.jpg", "spec"=>array("4GB", "5G", "5150mAH", "256GB"))
-        , array("SKU"=>"AX001", "Merk"=>"Xiaomi", "Model"=>"Mi 11 Pro", "Harga" => 9500000, "url_gambar"=>"https://p-id.ipricegroup.com/uploaded_dd537f8208714187b5acd25c34e8eb27.jpg", "spec"=>array("8GB", "5G", "5050mAH", "256GB"))
-        , array("SKU"=>"AR001", "Merk"=>"Redmi", "Model"=>"Note 12", "Harga" => 6500000, "url_gambar"=>"https://t-2.tstatic.net/shopping/foto/bank/images/redmi-note-12-ice-blue.jpg", "spec"=>array("6GB", "5G", "5350mAH", "256GB"))
-        , array("SKU"=>"AO001", "Merk" => "Oppo", "Model"=>"Reno 10", "Harga"=>4999000, "url_gambar"=>"https://image.oppo.com/content/dam/oppo/common/mkt/v2-2/reno10-5g-en/specs/reno10-5g-860_720.png", "spec"=>array("6GB", "5G", "6000mAH", "256GB"))
-        , array("SKU"=>"AS002", "Merk"=>"Samsung", "Model"=>"Galaxy S10 Plus", "Harga"=>12000000, "url_gambar"=>"https://images-cdn.ubuy.co.id/633ade5540042c54db49f4a3-samsung-galaxy-s10-plus-verizon-gsm.jpg", "spec"=>array("8GB", "4G", "4000mAH", "256GB"))
-        , array("SKU"=>"AX002", "Merk"=>"Xiaomi", "Model"=>"POCO F3", "Harga"=>5000000, "url_gambar"=>"https://www.static-src.com/wcsstore/Indraprastha/images/catalog/full//103/MTA-39494370/xiaomi_xiaomi_poco_f3_full01_bcac0724.jpg", "spec"=>array("8GB", "4G", "6000mAH", "256GB"))
-        , array("SKU"=>"AR002", "Merk"=>"Redmi", "Model"=>"Note 11 Pro 5G", "Harga"=>2500000, "url_gambar"=>"https://www.static-src.com/wcsstore/Indraprastha/images/catalog/full//100/MTA-57445516/br-m036969-03591_xiaomi-redmi-note-11-ram-6-rom-128gb_full01.jpg", "spec"=>array("8GB", "5G", "5000mAH", "256GB"))
-        , array("SKU"=>"AO002", "Merk" => "Oppo", "Model"=>"Reno 11 5G", "Harga"=>5999000, "url_gambar"=>"https://www.static-src.com/wcsstore/Indraprastha/images/catalog/full/catalog-image/MTA-153397392/oppo_oppo_reno_11_5g_8gb-256gb_full01_d0xf46hy.jpg", "spec"=>array("8GB", "5G", "6000mAH", "256GB"))
-        , array("SKU"=>"AS003", "Merk"=>"Samsung", "Model"=>"Galaxy A35 5G", "Harga" => 5000000, "url_gambar"=>"https://www.static-src.com/wcsstore/Indraprastha/images/catalog/full/catalog-image/95/MTA-164424019/samsung_samsung-a35-5g-smartphone-8gb-8gb-256gb-new-garansi-resmi_full01.jpg", "spec"=>array("8GB", "5G", "5000mAH", "256GB"))
-        , array("SKU"=>"AX003", "Merk"=>"Xiaomi", "Model"=>"12", "Harga" => 9999000, "url_gambar"=>"https://i01.appmifile.com/webfile/globalimg/id/cms/E7F6E977-AEE6-BACE-29FD-EF152669786F!800x800!85.jpg", "spec"=>array("8GB", "5G", "6500mAH", "256GB"))
-        , array("SKU"=>"AR003", "Merk"=>"Redmi", "Model"=>"Note 13", "Harga"=>2999000, "url_gambar"=>"https://i02.appmifile.com/409_item_id/19/02/2024/b1d78e465506da1d6e42fd3a627e7bbd!800x800!85.png" ,"spec"=>array("8GB", "5G", "5000mAH", "256GB"))
-        , array("SKU"=>"AO003", "Merk" => "Oppo", "Model"=>"A17", "Harga"=>1950000, "url_gambar"=>"https://images.tokopedia.net/img/cache/500-square/VqbcmM/2023/3/14/e237dc37-68ec-48bd-aa19-e6281e2cb98d.jpg", "spec"=>array("4GB", "4G", "5000mAH", "64GB"))
-        , array("SKU"=>"AS004", "Merk"=>"Samsung", "Model"=>"Galaxy A24", "Harga"=>3299000, "url_gambar"=>"https://www.static-src.com/wcsstore/Indraprastha/images/catalog/full//catalog-image/107/MTA-114994835/samsung_samsung_galaxy_a24_lte_ram_8-128_gb_garansi_resmi_new_full01_2d6c6def.jpg", "spec"=>array("6GB", "5G", "5000mAH", "256GB"))
-        , array("SKU"=>"AX004", "Merk"=>"Xiaomi", "Model"=>"14", "Harga" => 11999000, "url_gambar"=>"https://p-id.ipricegroup.com/uploaded_971b47165ef952e855fc54207f59b0d084aef440.jpg", "spec"=>array("8GB", "5G", "4610mAH", "256GB"))
-        , array("SKU"=>"AR004", "Merk"=>"Redmi", "Model"=>"12 Pro 5G", "Harga"=>3999000, "url_gambar"=>"https://www.static-src.com/wcsstore/Indraprastha/images/catalog/full//catalog-image/103/MTA-109570367/xiaomi_xiaomi_redmi_note_12_pro_5g_full05_fh4ohls9.jpg" ,"spec"=>array("6GB", "5G", "5000mAH", "256GB"))
-        , array("SKU"=>"AO004", "Merk" => "Oppo", "Model"=>"A98 5G", "Harga"=>3499000, "url_gambar"=>"https://www.static-src.com/wcsstore/Indraprastha/images/catalog/full//catalog-image/98/MTA-139565410/oppo_oppo_a98_5g_smartphone_-8gb-256gb-_full03_fcbkgtev.jpg", "spec"=>array("6GB", "5G", "5000mAH", "256GB"))
-        , array("SKU"=>"AS005", "Merk"=>"Samsung", "Model"=>"Galaxy S21 FE", "Harga"=>9999000, "url_gambar"=>"https://www.static-src.com/wcsstore/Indraprastha/images/catalog/full//108/MTA-95639066/samsung_samsung_galaxy_s21_fe_smartphone_-8-256gb-_full07_e6311212.jpg","spec"=>array("6GB", "5G", "5000mAH", "256GB"))
-        , array("SKU"=>"AX005", "Merk"=>"Xiaomi", "Model"=>"POCO M3", "Harga" => 2469000, "url_gambar"=>"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQa9CZfTHq8xeemqUhx-hqynbWrvvALWy_0Jw&s", "spec"=>array("4GB", "4G", "6000mAH", "128GB"))
-        , array("SKU"=>"AR005", "Merk"=>"Redmi", "Model"=>"Note 12", "Harga"=>3729000, "url_gambar"=>"https://i01.appmifile.com/webfile/globalimg/id/cms/3E7C39EE-E38A-31B1-B9FC-06246776D87B!800x800!85.jpg" ,"spec"=>array("6GB", "5G", "5000mAH", "256GB"))
-        , array("SKU"=>"AO004", "Merk" => "Oppo", "Model"=>"A78 5G", "Harga"=>3999000, "url_gambar"=>"https://www.static-src.com/wcsstore/Indraprastha/images/catalog/full//catalog-image/107/MTA-137982338/oppo_hp-oppo-a78-5g-8-128-gb-5000mah_full9.jpg", "spec"=>array("8GB", "5G", "5000mAH", "128GB"))       
-    );
+<?php
+$arrHape = array(
+    array("SKU" => "AS001", "Merk" => "Samsung", "Model" => "Galaxy S22", "Harga" => 18000000, "url_gambar" => "https://static.retailworldvn.com/Products/Images/12220/301427/smartphone-samsung-galaxy-s22-5g-8gb-128gb-black-1.jpg", "spec" => array("4GB", "5G", "5150mAH", "256GB")), 
+    array("SKU" => "AX001", "Merk" => "Xiaomi", "Model" => "Mi 11 Pro", "Harga" => 9500000, "url_gambar" => "https://p-id.ipricegroup.com/uploaded_dd537f8208714187b5acd25c34e8eb27.jpg", "spec" => array("8GB", "5G", "5050mAH", "256GB")), 
+    array("SKU" => "AR001", "Merk" => "Redmi", "Model" => "Note 12", "Harga" => 6500000, "url_gambar" => "https://t-2.tstatic.net/shopping/foto/bank/images/redmi-note-12-ice-blue.jpg", "spec" => array("6GB", "5G", "5350mAH", "256GB")), 
+    array("SKU" => "AO001", "Merk" => "Oppo", "Model" => "Reno 10", "Harga" => 4999000, "url_gambar" => "https://image.oppo.com/content/dam/oppo/common/mkt/v2-2/reno10-5g-en/specs/reno10-5g-860_720.png", "spec" => array("6GB", "5G", "6000mAH", "256GB")), 
+    array("SKU" => "AS002", "Merk" => "Samsung", "Model" => "Galaxy S10 Plus", "Harga" => 12000000, "url_gambar" => "https://images-cdn.ubuy.co.id/633ade5540042c54db49f4a3-samsung-galaxy-s10-plus-verizon-gsm.jpg", "spec" => array("8GB", "4G", "4000mAH", "256GB")), 
+    array("SKU" => "AX002", "Merk" => "Xiaomi", "Model" => "POCO F3", "Harga" => 5000000, "url_gambar" => "https://www.static-src.com/wcsstore/Indraprastha/images/catalog/full//103/MTA-39494370/xiaomi_xiaomi_poco_f3_full01_bcac0724.jpg", "spec" => array("8GB", "4G", "6000mAH", "256GB")), 
+    array("SKU" => "AR002", "Merk" => "Redmi", "Model" => "Note 11 Pro 5G", "Harga" => 2500000, "url_gambar" => "https://www.static-src.com/wcsstore/Indraprastha/images/catalog/full//100/MTA-57445516/br-m036969-03591_xiaomi-redmi-note-11-ram-6-rom-128gb_full01.jpg", "spec" => array("8GB", "5G", "5000mAH", "256GB")), 
+    array("SKU" => "AO002", "Merk" => "Oppo", "Model" => "Reno 11 5G", "Harga" => 5999000, "url_gambar" => "https://www.static-src.com/wcsstore/Indraprastha/images/catalog/full/catalog-image/MTA-153397392/oppo_oppo_reno_11_5g_8gb-256gb_full01_d0xf46hy.jpg", "spec" => array("8GB", "5G", "6000mAH", "256GB")), 
+    array("SKU" => "AS003", "Merk" => "Samsung", "Model" => "Galaxy A35 5G", "Harga" => 5000000, "url_gambar" => "https://www.static-src.com/wcsstore/Indraprastha/images/catalog/full/catalog-image/95/MTA-164424019/samsung_samsung-a35-5g-smartphone-8gb-8gb-256gb-new-garansi-resmi_full01.jpg", "spec" => array("8GB", "5G", "5000mAH", "256GB")), 
+    array("SKU" => "AX003", "Merk" => "Xiaomi", "Model" => "12", "Harga" => 9999000, "url_gambar" => "https://i01.appmifile.com/webfile/globalimg/id/cms/E7F6E977-AEE6-BACE-29FD-EF152669786F!800x800!85.jpg", "spec" => array("8GB", "5G", "6500mAH", "256GB")), 
+    array("SKU" => "AR003", "Merk" => "Redmi", "Model" => "Note 13", "Harga" => 2999000, "url_gambar" => "https://i02.appmifile.com/409_item_id/19/02/2024/b1d78e465506da1d6e42fd3a627e7bbd!800x800!85.png", "spec" => array("8GB", "5G", "5000mAH", "256GB")), 
+    array("SKU" => "AO003", "Merk" => "Oppo", "Model" => "A17", "Harga" => 1950000, "url_gambar" => "https://images.tokopedia.net/img/cache/500-square/VqbcmM/2023/3/14/e237dc37-68ec-48bd-aa19-e6281e2cb98d.jpg", "spec" => array("4GB", "4G", "5000mAH", "64GB")), 
+    array("SKU" => "AS004", "Merk" => "Samsung", "Model" => "Galaxy A24", "Harga" => 3299000, "url_gambar" => "https://www.static-src.com/wcsstore/Indraprastha/images/catalog/full//catalog-image/107/MTA-114994835/samsung_samsung_galaxy_a24_lte_ram_8-128_gb_garansi_resmi_new_full01_2d6c6def.jpg", "spec" => array("6GB", "5G", "5000mAH", "256GB")), 
+    array("SKU" => "AX004", "Merk" => "Xiaomi", "Model" => "14", "Harga" => 11999000, "url_gambar" => "https://p-id.ipricegroup.com/uploaded_971b47165ef952e855fc54207f59b0d084aef440.jpg", "spec" => array("8GB", "5G", "4610mAH", "256GB")), 
+    array("SKU" => "AR004", "Merk" => "Redmi", "Model" => "12 Pro 5G", "Harga" => 3999000, "url_gambar" => "https://www.static-src.com/wcsstore/Indraprastha/images/catalog/full//catalog-image/103/MTA-109570367/xiaomi_xiaomi_redmi_note_12_pro_5g_full05_fh4ohls9.jpg", "spec" => array("6GB", "5G", "5000mAH", "256GB")), 
+    array("SKU" => "AO004", "Merk" => "Oppo", "Model" => "A98 5G", "Harga" => 3499000, "url_gambar" => "https://www.static-src.com/wcsstore/Indraprastha/images/catalog/full//catalog-image/98/MTA-139565410/oppo_oppo_a98_5g_smartphone_-8gb-256gb-_full03_fcbkgtev.jpg", "spec" => array("6GB", "5G", "5000mAH", "256GB")), 
+    array("SKU" => "AS005", "Merk" => "Samsung", "Model" => "Galaxy S21 FE", "Harga" => 9999000, "url_gambar" => "https://www.static-src.com/wcsstore/Indraprastha/images/catalog/full//108/MTA-95639066/samsung_samsung_galaxy_s21_fe_smartphone_-8-256gb-_full07_e6311212.jpg", "spec" => array("6GB", "5G", "5000mAH", "256GB")), 
+    array("SKU" => "AX005", "Merk" => "Xiaomi", "Model" => "POCO M3", "Harga" => 2469000, "url_gambar" => "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQa9CZfTHq8xeemqUhx-hqynbWrvvALWy_0Jw&s", "spec" => array("4GB", "4G", "6000mAH", "128GB")), 
+    array("SKU" => "AR005", "Merk" => "Redmi", "Model" => "Note 12", "Harga" => 3729000, "url_gambar" => "https://i01.appmifile.com/webfile/globalimg/id/cms/3E7C39EE-E38A-31B1-B9FC-06246776D87B!800x800!85.jpg", "spec" => array("6GB", "5G", "5000mAH", "256GB")), 
+    array("SKU" => "AO004", "Merk" => "Oppo", "Model" => "A78 5G", "Harga" => 3999000, "url_gambar" => "https://www.static-src.com/wcsstore/Indraprastha/images/catalog/full//catalog-image/107/MTA-137982338/oppo_hp-oppo-a78-5g-8-128-gb-5000mah_full9.jpg", "spec" => array("8GB", "5G", "5000mAH", "128GB"))
+);
 
 ?>
 <!DOCTYPE html>
 <html>
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -34,11 +35,12 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap" rel="stylesheet">
     <style type="text/css">
-        input[type='checkbox']:checked{
+        input[type='checkbox']:checked {
             background-color: orangered;
         }
     </style>
 </head>
+
 <body>
     <div class="container">
         <div id="navbar">
@@ -51,28 +53,28 @@
         <div id="content">
             <div class="card-container">
                 <form action="bandingkan.php" method="post">
-                    <?php foreach ($arrHape as $key => $value) :?>
+                    <?php foreach ($arrHape as $key => $value) : ?>
                         <div class="card">
-                            <img src="<?php echo $value['url_gambar']?>" class="card-img">
+                            <img src="<?php echo $value['url_gambar'] ?>" class="card-img">
                             <div class="card-description">
                                 <p class="card-title">
-                                    <?php echo $value['Merk'] . " " . $value['Model'];?>  
+                                    <?php echo $value['Merk'] . " " . $value['Model']; ?>
                                 </p>
-                                
-                                <p id="price"><span class="rp">Rp </span><?php echo $value['Harga'];?></p>
+
+                                <p id="price"><span class="rp">Rp </span><?php echo $value['Harga']; ?></p>
                                 <div class="spec">
                                     <p>Spesifikasi</p>
                                     <ul class="card-spec">
-                                        <li>RAM : <span><?php echo $value['spec'][0]?></span></li>
-                                        <li>Internet : <span><?php echo $value['spec'][1]?></span></li>
-                                        <li>Baterai : <span><?php echo $value['spec'][2]?></span></li>
-                                        <li>Storage : <span><?php echo $value['spec'][3]?></span></li>
+                                        <li>RAM : <span><?php echo $value['spec'][0] ?></span></li>
+                                        <li>Internet : <span><?php echo $value['spec'][1] ?></span></li>
+                                        <li>Baterai : <span><?php echo $value['spec'][2] ?></span></li>
+                                        <li>Storage : <span><?php echo $value['spec'][3] ?></span></li>
                                     </ul>
                                 </div>
-                                
+
                             </div>
                             <div class="checkbox-container">
-                                <input class="checkbox" type="checkbox" name="hape[]" value="<?php echo $value['SKU']; ?>" id="hape<?php echo $value['SKU'];?>">
+                                <input class="checkbox" type="checkbox" name="hape[]" value="<?php echo $value['SKU']; ?>" id="hape<?php echo $value['SKU']; ?>">
                                 <label for="hape<?php echo $value['SKU']; ?>">Pilih</label>
                             </div>
                             <input type="hidden" name="merk_<?php echo $value['SKU']; ?>" value="<?php echo $value['Merk']; ?>">
@@ -83,13 +85,13 @@
                             <input type="hidden" name="internet_<?php echo $value['SKU']; ?>" value="<?php echo $value['spec'][1]; ?>">
                             <input type="hidden" name="baterai_<?php echo $value['SKU']; ?>" value="<?php echo $value['spec'][2]; ?>">
                             <input type="hidden" name="storage_<?php echo $value['SKU']; ?>" value="<?php echo $value['spec'][3]; ?>">
-                            
-                        </div>   
-                    <?php endforeach;?>
+
+                        </div>
+                    <?php endforeach; ?>
                     <input type="submit" name="submit" value="COMPARE" id="btnBanding">
                 </form>
             </div>
-        </div>  
+        </div>
     </div>
     <script src="js/jquery-3.5.1.min.js"></script>
     <script type="text/javascript">
@@ -99,27 +101,37 @@
                 if (checkedCount === 3) {
                     $("#btnBanding").removeAttr("disabled");
                     $('input[type="checkbox"]:not(:checked)').attr("disabled", true);
-                }
-                else{
+                } else {
                     $("#btnBanding").attr("disabled", true);
                     $('input[type="checkbox"]').removeAttr("disabled");
                 }
             }
-        
+
             // Initially update count on page load
             updateCheckedCount();
-            
+
             // Attach change event handler to checkboxes
             $('input[type="checkbox"]').change(function() {
                 // Call updateCheckedCount function when checkbox state changes
                 updateCheckedCount();
             });
 
+            // Check and apply dark mode from local storage
+            if (localStorage.getItem('darkMode') === 'enabled') {
+                $('body').addClass('dark-mode');
+            }
+
             // Toggle dark mode
             $('#toggle-dark-mode').click(function() {
                 $('body').toggleClass('dark-mode');
+                if ($('body').hasClass('dark-mode')) {
+                    localStorage.setItem('darkMode', 'enabled');
+                } else {
+                    localStorage.setItem('darkMode', 'disabled');
+                }
             });
         });
     </script>
 </body>
+
 </html>
